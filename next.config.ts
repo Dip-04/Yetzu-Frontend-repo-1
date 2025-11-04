f79ccdd66e8419138da7cc9ee5a3f5741a392d55
Mr Dev <diptish.gohane@mizzendigital.com>
2025-11-04 14:19:09 +0530

    img fixes

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,18 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "productionyetzuapi.yetzu.com",
       },
+      {
+        protocol: "https",
+        hostname: "yetzu.com",
+      },
+      {
+        protocol: "http",
+        hostname: "157.173.220.125",
+      },
     ],
+    unoptimized: true, // Important if you serve static images from public/
   },
+  outputFileTracingRoot: __dirname, // Fix Next.js workspace root warning
 };
 
 export default nextConfig;
